3a44e7c61d36128454a0e7e8856119d803cfe315
Anirban Maiti <amaity885@gmail.com>
2019-03-19 19:09:12 +0530

    adding sockets

--- index.js
+++ index.js
@@ -15,5 +15,9 @@ app.use(express.static('public'));
 var io = socket(server);
 
 io.on('connection',function(socket){
-  console.log('made socket connection', socket.id)
+  console.log('made socket connection', socket.id);
+
+  socket.on('chat',function(data){
+    io.sockets.emit('chat',data);
+  });
 });
