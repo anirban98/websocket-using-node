d568cfcaea01fc725627b5e7a843050ec489dd4a
Anirban Maiti <amaity885@gmail.com>
2019-03-19 19:13:41 +0530

    final

--- public/chat.js
+++ public/chat.js
@@ -1,24 +1,22 @@
-//make connection
-
+// Make connection
 var socket = io.connect('http://localhost:4000');
 
-//Query DOM
-var message = document.getElementById('message');
-    handle = document.getElementById('handle'),
-    btn = document.getElementById('send'),
-    output = document.getElementById('output');
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
 
-//EMIT events
-
-btn.addEventListner('click',function(){
-  socket.emit('chat',{
-    message: message.value,
-    handle: handle.value
+// Emit events
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
   });
+  message.value = "";
 });
 
-//listen for events
-
-socket.on('chat',function(data){
-  output.innerHTML +='<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+// Listen for events
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
