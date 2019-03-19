4e14a479d20eaf9a43fba9ca4471a9d77eb2b4d7
Anirban Maiti <amaity885@gmail.com>
2019-03-19 18:19:08 +0530

    connection setup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var socket = require('socket.io');
 //App setup
 var app = express();
 var server = app.listen(4000,function(){
@@ -9,3 +9,11 @@ var server = app.listen(4000,function(){
 
 //static files
 app.use(express.static('public'));
+
+
+//socket setup
+var io = socket(server);
+
+io.on('connection',function(socket){
+  console.log('made socket connection', socket.id)
+});
